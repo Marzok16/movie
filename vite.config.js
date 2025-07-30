28ec64fba5972700c4651bd7d52bb2cbd8715e2c
Marzok16 <abdulrhmanmarzok16@gmail.com>
2025-07-30 15:49:11 +0300

    Fix Vite base and index.html for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/movie-app/', // 👈 this should match your repo name
+  base: '/movie-app/', // This must match your repo name!
   plugins: [react()],
 });
