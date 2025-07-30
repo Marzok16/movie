c310b8be36e433d9cfe34b1c03adc51edb5ab441
Marzok16 <abdulrhmanmarzok16@gmail.com>
2025-07-30 15:58:57 +0300

    Fix Vite base and index.html for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./store/store";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 
-// make sure this path is correct
+// Import Bootstrap styles
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+// Use 'basename' for GitHub Pages deployment
+const BASENAME = "/marzok16.github.io"; // replace with your repo name if it's different
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/movie-app">
         <App />
       </BrowserRouter>
     </Provider>
